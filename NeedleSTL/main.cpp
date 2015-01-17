56c0756a0e80fd7d1b7ccef57c447d5364803f5d
mayingjie <mayingjie116@gmail.com>
2015-01-17 12:19:53 -0500

    Initial Commit

---
+++ NeedleSTL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NeedleSTL
+//
+//  Created by Yingjie Ma on 1/17/15.
+//  Copyright (c) 2015 YingJ Ma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
